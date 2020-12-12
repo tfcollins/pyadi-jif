9ff4175ad0820f6f6bdd768c1e2edca79bd29097
Travis F. Collins <travis.collins@analog.com>
2020-12-11 16:26:30 -0800

    Fix AD9523-1 model to reflect feedback divider correctly
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- adijif/clocks/ad9523.py
+++ adijif/clocks/ad9523.py
@@ -13,10 +13,12 @@ class ad9523_1(clock):
     vco_min = 2.94e9
     vco_max = 3.1e9
 
+    pfb_max = 259e6
+
     use_vcxo_double = False
 
     """ VCXO multiplier """
-    n_available = [12, 16, 17, 20, 21, 22, 24, 25, 26, *range(28, 255)]
+    n2_available = [12, 16, 17, 20, 21, 22, 24, 25, 26, *range(28, 255)]
     a_available = range(0, 3)
     b_available = range(3, 63)
     # N = (PxB) + A, P=4, A==[0,1,2,3], B=[3..63]
@@ -59,45 +61,53 @@ class ad9523_1(clock):
         mod = np.gcd.reduce(np.array(required_output_rates, dtype=int))
         configs = []
 
-        for r2 in self.r2_available:
-            vco = vcxo * r2
-            if vco > self.vco_min and vco < self.vco_max:
-                for m1 in self.m12_available:
-                    # print("vco",vco,mod,m1)
-                    if (vco / m1) % mod == 0:
-                        # See if we can use only m1 and not both m1+m2
-                        required_output_divs = (vco / m1) / required_output_rates
-                        if np.all(np.in1d(required_output_divs, self.d_available)):
-                            configs.append(
-                                {
-                                    "m1": m1,
-                                    "vco": vco,
-                                    "r2": r2,
-                                    "required_output_divs": required_output_divs,
-                                }
-                            )
-                        else:
-                            # Try to use m2 as well to meet required out clocks
-                            f1 = np.in1d(required_output_divs, self.d_available)
-                            for m2 in self.m12_available:
-                                required_output_divs2 = (
-                                    vco / m2
-                                ) / required_output_rates
-                                f2 = np.in1d(required_output_divs2, self.d_available)
-                                if np.logical_or(f1, f2).all():
-                                    configs.append(
-                                        {
-                                            "m1": m1,
-                                            "m2": m2,
-                                            "vco": vco,
-                                            "r2": r2,
-                                            "required_output_divs": required_output_divs[
-                                                f1
-                                            ].tolist(),
-                                            "required_output_divs2": required_output_divs2[
-                                                f2
-                                            ].tolist(),
-                                        }
+        for n2 in self.n2_available:
+            for r2 in self.r2_available:
+                pfb = vcxo / r2
+                if pfb > self.pfb_max:
+                    continue
+                vco = pfb * n2
+                if vco > self.vco_min and vco < self.vco_max:
+                    for m1 in self.m12_available:
+                        # print("vco",vco,mod,m1)
+                        if (vco / m1) % mod == 0:
+                            # See if we can use only m1 and not both m1+m2
+                            required_output_divs = (vco / m1) / required_output_rates
+                            if np.all(np.in1d(required_output_divs, self.d_available)):
+                                configs.append(
+                                    {
+                                        "m1": m1,
+                                        "n2": n2,
+                                        "vco": vco,
+                                        "r2": r2,
+                                        "required_output_divs": required_output_divs,
+                                    }
+                                )
+                            else:
+                                # Try to use m2 as well to meet required out clocks
+                                f1 = np.in1d(required_output_divs, self.d_available)
+                                for m2 in self.m12_available:
+                                    required_output_divs2 = (
+                                        vco / m2
+                                    ) / required_output_rates
+                                    f2 = np.in1d(
+                                        required_output_divs2, self.d_available
                                     )
+                                    if np.logical_or(f1, f2).all():
+                                        configs.append(
+                                            {
+                                                "m1": m1,
+                                                "m2": m2,
+                                                "n2": n2,
+                                                "vco": vco,
+                                                "r2": r2,
+                                                "required_output_divs": required_output_divs[
+                                                    f1
+                                                ].tolist(),
+                                                "required_output_divs2": required_output_divs2[
+                                                    f2
+                                                ].tolist(),
+                                            }
+                                        )
 
         return configs
